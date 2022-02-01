f8db44b5faac50ca05fd50d700eafe7cb4a7489e
Keylane <keylanessilva07@gmail.com>
2022-02-01 02:34:25 -0300

    ultimos ajustes nos projetos

--- js/index.js
+++ js/index.js
@@ -28,21 +28,7 @@ $(document).ready(function(){
     })
 })
 
-// function typeWrite(elemento){
-//   const textoArray = elemento.innerHTML.split('');
-//   elemento.innerHTML = ' ';
-//   textoArray.forEach(function(letra, i){   
-    
-//   setTimeout(function(){
-//       elemento.innerHTML += letra;
-//   }, 75 * i)
-
-// });
-// }
-
-// const titulo = document.querySelector('#title-nome');
-// typeWrite(titulo);
-
+// ParticlesJS
 
 particlesJS("particles-container", {
     particles: {
